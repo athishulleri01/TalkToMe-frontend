212f93c113564354ebbe7d78cef166663bfebd50
athishulleri01 <athishulleri0000@gmail.com>
2024-07-02 23:46:25 +0530

    configured axios

--- src/axiosConfig.jsx
+++ src/axiosConfig.jsx
@@ -1,4 +1,2 @@
-// export const AUTH_URL =  "http://localhost:8005/user-service/api/v1"
-// export const AUTH_URL =  "http://127.0.0.1:8001/api/login/"
 export const AUTH_URL =  "http://127.0.0.1:8000"
 
